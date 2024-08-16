7bdfa210def5174f42034737c5bbf4ea51389651
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-08-16 09:51:30 +0100

    feat: Add contact page component

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,20 @@
+const Contact = () => {
+  return (
+    <div className="h-screen bg-red-500 pt-20">
+      <h1 className="pl-10">Contact</h1>
+      <div className=" px-20 pl-[28rem]">
+        <p className="pr-5 leading-relaxed text-lg">
+          <b>Lorem ipsum</b> dolor sit amet, consectetur adipisicing elit. <b>Animi</b> ipsa
+          cupiditate excepturi tempore quo quae repellendus nam modi, id fugit
+          molestiae numquam velit commodi porro voluptas exercitationem in nobis
+          error quia quas similique voluptatibus.
+          <br /> Voluptatem dolore officia nulla ut aut. Lorem ipsum dolor sit
+          amet consectetur, adipisicing elit. Sequi aperiam veniam voluptatibus
+          ea. <b>Blanditiis ex quasi est, id eos odio!</b>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
