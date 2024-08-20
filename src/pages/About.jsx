93c0cc5756f01eb947743927318f0cee5e802941
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-08-20 13:30:59 +0100

    feat: Add lazy loading for images in About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,17 +1,28 @@
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+
 const About = () => {
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate("register");
+    };
+    const location = useLocation();
+
+    console.log(location);
   return (
     <div className="h-screen bg-yellow-300 pt-20">
+      <Outlet />
       <h1 className="pl-10">About</h1>
       <div className=" px-20 pl-[28rem]">
         <p className="pr-5 leading-relaxed text-lg">
-          <b>Lorem ipsum</b> dolor sit amet, consectetur adipisicing elit. <b>Animi</b> ipsa
-          cupiditate excepturi tempore quo quae repellendus nam modi, id fugit
-          molestiae numquam velit commodi porro voluptas exercitationem in nobis
-          error quia quas similique voluptatibus.
+          <b>Lorem ipsum</b> dolor sit amet, consectetur adipisicing elit.{" "}
+          <b>Animi</b> ipsa cupiditate excepturi tempore quo quae repellendus
+          nam modi, id fugit molestiae numquam velit commodi porro voluptas
+          exercitationem in nobis error quia quas similique voluptatibus.
           <br /> Voluptatem dolore officia nulla ut aut. Lorem ipsum dolor sit
           amet consectetur, adipisicing elit. Sequi aperiam veniam voluptatibus
           ea. <b>Blanditiis ex quasi est, id eos odio!</b>
         </p>
+        <button onClick={handleClick} className="bg-white self-end rounded-lg px-3 py-1 mt-2 font-semibold">register</button>
       </div>
     </div>
   );
---
+++ src/pages/Blog.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import data from "../assets/mockData.json";
+import BlogCard from "../components/BlogCard";
+import { Outlet } from "react-router-dom";
+
+const Blog = () => {
+  const [blogPosts] = useState(data?.blogPosts);
+  return (
+    <div>
+      <h1>Blog</h1>
+      <div className="grid grid-cols-3 px-16 gap-10 justify-items-center py-10">
+        {blogPosts.map((post) => (
+          <BlogCard key={post.id} post={post} />
+        ))}
+      </div>
+
+    </div>
+  );
+};
+
+export default Blog;
