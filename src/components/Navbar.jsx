5cd2f981959e4f1dcb8fdeff73df75dc422b6a5b
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-08-20 09:49:23 +0100

    chore: Add Navbar component and styling

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="absolute w-full top-3 ">
+      <ul className="flex justify-around text-2xl">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link
+            to={{
+              pathname: "/settings",
+              search: "?sort=date",
+              state: { fromHome: true },
+            }}
+          >
+            Settings
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
