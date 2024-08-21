40b14b677e243ae6be66fabd2e22b9e4b5b21fc4
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-08-21 12:26:50 +0100

    feat: Add useLocation hook to ThemeProvider component for dynamic styling

--- src/contexts/ThemeProvider.jsx
+++ src/contexts/ThemeProvider.jsx
@@ -2,11 +2,16 @@
 
 import { useState } from "react";
 import ThemeContext from "./ThemeContext";
+import { useLocation } from "react-router-dom";
 
 const ThemeProvider = ({ children }) => {
   const [darkMode, setDarkMode] = useState(false);
+  const location = useLocation();
+  const { pathname } = location;
+  console.log(location);
+
   return (
-    <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+    <ThemeContext.Provider value={{ darkMode, setDarkMode, pathname }}>
       {children}
     </ThemeContext.Provider>
   );
