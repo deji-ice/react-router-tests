782f80e9fdf2cc71a487eaa15964ab8568107c78
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-08-22 16:02:49 +0100

    style: Update font families in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      aeonik_light: ["Aeonik-light", "sans-serif"],
+      aeonik_regular: ["Aeonik-regular", "sans-serif"],
+      aeonik_bold: ["Aeonik-bold", "sans-serif"],
+    },
   },
   plugins: [],
-}
+};
