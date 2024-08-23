653f713baab1142dbd5cfcfe088ff447a3f21b6f
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-08-24 00:10:17 +0100

    refactor: Remove unused import statement in Error.jsx

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import ThemeContext from "../contexts/ThemeContext";
 
 export const Error404 = () => {
