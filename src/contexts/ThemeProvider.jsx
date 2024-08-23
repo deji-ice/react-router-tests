abab10e76e460d25b59206aafd20fb70714acf82
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-08-23 16:14:40 +0100

    feat: Add localStorage support for dark mode in ThemeProvider.jsx

--- src/contexts/ThemeProvider.jsx
+++ src/contexts/ThemeProvider.jsx
@@ -1,13 +1,24 @@
 /* eslint-disable react/prop-types */
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ThemeContext from "./ThemeContext";
 import { useLocation } from "react-router-dom";
 
 const ThemeProvider = ({ children }) => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(() => {
+    // Get the user from localStorage if it exists
+    const savedTheme = localStorage.getItem("darkMode");
+    return savedTheme ? JSON.parse(savedTheme) : false;
+  });
+  // Use useEffect to update localStorage whenever the theme changes
+  useEffect(() => {
+    localStorage.setItem("darkMode", JSON.stringify(darkMode));
+    console.log(localStorage.getItem("darkMode"));
+  }, [darkMode]);
+
   const location = useLocation();
   const { pathname } = location;
+
   console.log(location);
 
   return (
